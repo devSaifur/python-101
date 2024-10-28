6c18aeb781eb805fb63d726cef336dec7814cab9
devSaifur <saifur2akash@gmail.com>
2024-10-28 18:49:09 +0600

    scope

---
+++ scope/scope.py
@@ -0,0 +1,14 @@
+def get_fullname():
+    first_name = "Saifur"
+    last_name = "Rahman"
+    fullname = f"{first_name} {last_name}"
+    return fullname
+
+
+def show_fullname():
+    print(fullname)
+
+
+fullname = get_fullname()
+
+show_fullname()
