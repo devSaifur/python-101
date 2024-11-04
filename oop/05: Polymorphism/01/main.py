b0a56f181a41ec3b20bd5e0fa63129b60ee16f67
devSaifur <saifur2akash@gmail.com>
2024-11-04 17:18:52 +0600

    polymorphism

---
+++ oop/05: Polymorphism/01/main.py
@@ -0,0 +1,56 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x1, y1, x2, y2):
+        pass
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, height, width, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.fire_range = fire_range
+        self.height = height
+        self.width = width
+
+        self.__hit_box = Rectangle(
+            pos_x - width / 2, pos_y - height / 2, pos_x + width / 2, pos_y + height / 2
+        )
+        pass
+
+    def in_area(self, x1, y1, x2, y2):
+        area_rect = Rectangle(x1, y1, x2, y2)
+        return self.__hit_box.overlaps(area_rect)
+
+
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_left_x(self):
+        return min(self.__x1, self.__x2)
+
+    def get_right_x(self):
+        return max(self.__x1, self.__x2)
+
+    def get_top_y(self):
+        return max(self.__y1, self.__y2)
+
+    def get_bottom_y(self):
+        return min(self.__y1, self.__y2)
+
+    def overlaps(self, rect):
+        return (
+            self.get_left_x() <= rect.get_right_x()
+            and self.get_right_x() >= rect.get_left_x()
+            and self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
+        )
+
+    def __repr__(self):
+        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
